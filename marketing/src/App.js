1fc9406b9aa922d06e3e82081bfd4dc11b33c2e9
Souraevshing <isauravshing@gmail.com>
2025-09-15 18:57:39 +0530

    perf: improve performance of both marketing and container

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import {Switch, Route, Router} from 'react-router-dom';
 import {createGenerateClassName, StylesProvider} from "@material-ui/core";
 
 import LandingPage from "./components/Landing";
@@ -10,17 +10,19 @@ const generateClassName = createGenerateClassName({
     productionPrefix: '_cls_marketing',
 });
 
-export default function App() {
+export default function App({history}) {
     return(
-        <StylesProvider generateClassName={generateClassName}>
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/" component={LandingPage} />
-                    <Route path="/auth/signup" component={LandingPage} />
-                    <Route exact path={'/pricing'} component={PricingPage} />
-                    <Route path={'*'} component={ErrorPage} />
-                </Switch>
-            </BrowserRouter>
-        </StylesProvider>
+        <div>
+            <StylesProvider generateClassName={generateClassName}>
+                <Router history={history}>
+                    <Switch>
+                        <Route exact path="/" component={LandingPage} />
+                        <Route path="/auth/signup" component={LandingPage} />
+                        <Route exact path={'/pricing'} component={PricingPage} />
+                        <Route path={'*'} component={ErrorPage} />
+                    </Switch>
+                </Router>
+            </StylesProvider>
+        </div>
     )
 }
