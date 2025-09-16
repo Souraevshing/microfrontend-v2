7dbe8d2386ea3f8e0c7151d53462e0699fcf14bb
Souraevshing <isauravshing@gmail.com>
2025-09-16 14:59:03 +0530

    feat: add auth impl in signin and signup

--- auth/src/App.js
+++ auth/src/App.js
@@ -9,14 +9,18 @@ const generateClassName = createGenerateClassName({
     productionPrefix: '_cls_auth',
 });
 
-export default ({history}) => {
+export default ({history, onSignIn}) => {
     return(
         <div>
             <StylesProvider generateClassName={generateClassName}>
                 <Router history={history}>
                     <Switch>
-                        <Route exact path="/auth/signin" component={SignIn} />
-                        <Route exact path="/auth/signup" component={SignUp} />
+                        <Route path="/auth/signin" component={SignIn}>
+                            <SignIn onSignIn={onSignIn} />
+                        </Route>
+                        <Route path="/auth/signup" component={SignUp}>
+                            <SignUp onSignIn={onSignIn} />
+                        </Route>
                     </Switch>
                 </Router>
             </StylesProvider>
